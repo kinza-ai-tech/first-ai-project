de52816c154875f17daa02a159efdec6671115ee
Kinza Nawaz <280600522+kinza-ai-tech@users.noreply.github.com>
2026-05-01 09:50:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-name = input("Enter your name: ")
+print("AI Chatbot 🤖")
+print("Type 'exit' to stop the chat\n")
 
-print("\nHello", name)
-print("Welcome to your AI Engineering journey 🚀")
-print("Keep building real projects 💡")
+while True:
+    user = input("You: ")
+
+    if user.lower() == "exit":
+        print("Bot: Goodbye! Keep learning AI 🚀")
+        break
+
+    elif "name" in user.lower():
+        print("Bot: My name is AI Assistant.")
+
+    elif "hello" in user.lower():
+        print("Bot: Hello! How can I help you?")
+
+    elif "how are you" in user.lower():
+        print("Bot: I am just code, but I'm doing great 🤖")
+
+    else:
+        print("Bot: I am still learning... 🤖")
